d170b2188c6c7fe141bfd3e3e8e5c3eaa883535f
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-30 16:57:25 -0400

    refactor: move sut creation to a factory method

--- src/presentation/pages/home/home.spec.tsx
+++ src/presentation/pages/home/home.spec.tsx
@@ -4,10 +4,22 @@ import { fireEvent, render, screen } from '@testing-library/react'
 import Home from './home'
 import { LoadMasterSpy } from '@/domain/test/mock-load-master'
 
+type SutTypes = {
+  loadMasterSpy: LoadMasterSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadMasterSpy = new LoadMasterSpy()
+  render(<Home loadMaster={loadMasterSpy} />)
+
+  return {
+    loadMasterSpy
+  }
+}
+
 describe('Home Component', () => {
   test('Should call LoadMaster on START button click', () => {
-    const loadMasterSpy = new LoadMasterSpy()
-    render(<Home loadMaster={loadMasterSpy} />)
+    const { loadMasterSpy } = makeSut()
     fireEvent.click(screen.getByTestId('start-button'))
     expect(loadMasterSpy.callsCount).toBe(1)
   })
