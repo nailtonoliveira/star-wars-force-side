6b79df577878742c88dcd7c42d3a3fce16a031ba
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-29 22:33:29 -0400

    refactor: move sut creation to a factory method

--- src/data/usecases/remote-load-master/remote-load-master.spec.ts
+++ src/data/usecases/remote-load-master/remote-load-master.spec.ts
@@ -1,12 +1,25 @@
 import { RemoteLoadMaster } from './remote-load-master'
 import { HttpGetClientSpy } from '@/data/test'
 
+type SutTypes = {
+  sut: RemoteLoadMaster
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new RemoteLoadMaster(httpGetClientSpy)
+  return {
+    sut,
+    httpGetClientSpy
+  }
+}
+
 describe('RemoteLoadMaster', () => {
   test('Should call HttpGetClient twice with correct urls', async () => {
     const url1 = '/people/1'
     const url2 = '/people/4'
-    const httpGetClientSpy = new HttpGetClientSpy()
-    const sut = new RemoteLoadMaster(httpGetClientSpy)
+    const { sut, httpGetClientSpy } = makeSut()
     await sut.load()
     expect(httpGetClientSpy.url).toEqual([url1, url2])
     expect(httpGetClientSpy.url[0]).toBe(url1)
