b18e1042f04ae82c51c2c0df019f86c645de1e5e
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-30 15:19:15 -0400

    feat: ensure AxiosHttpClient return axios.get with correct error

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,9 +1,15 @@
 import { HttpResponse } from '@/data/protocols/http'
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 export class AxiosHttpClient {
   async get(url: string): Promise<HttpResponse> {
-    const axiosResponse = await axios.get(url)
+    let axiosResponse: AxiosResponse
+    try {
+      axiosResponse = await axios.get(url)
+    } catch (error) {
+      axiosResponse = error.response
+    }
+
     return {
       statusCode: axiosResponse.status,
       body: axiosResponse.data
