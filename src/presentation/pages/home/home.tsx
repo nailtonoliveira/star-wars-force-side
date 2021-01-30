8fa5267a74205fc2cbc0c9f7a6887827819e8130
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-30 16:52:44 -0400

    feat: ensure call LoadMaster on START button click

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -1,14 +1,32 @@
+import { LoadMaster } from '@/domain/usecases'
 import React from 'react'
 import Styles from './home-styles.scss'
 
-const Home = (): JSX.Element => {
+type Props = {
+  loadMaster: LoadMaster
+}
+
+const Home = ({ loadMaster }: Props): JSX.Element => {
+  const handleStartClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    event.preventDefault()
+    loadMaster.load()
+  }
+
   return (
     <div className={Styles.homeWrap}>
       <span className={Styles.title}>
         Welcome to <strong>iClinic</strong>
       </span>
       <span className={Styles.subtitle}>Frontend Challenge</span>
-      <button type="button">Start</button>
+      <button
+        data-testid="start-button"
+        type="button"
+        onClick={handleStartClick}
+      >
+        Start
+      </button>
     </div>
   )
 }
