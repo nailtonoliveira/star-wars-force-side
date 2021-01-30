873d7f9dac2cdc73807c66d34e3264fa462f217a
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-30 12:16:32 -0400

    feat: ensure RemoteLoadMaster return a LoadMaster.Model correctly

--- src/data/usecases/remote-load-master/remote-load-master.ts
+++ src/data/usecases/remote-load-master/remote-load-master.ts
@@ -2,19 +2,19 @@ import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
 import { UnexpectedError } from '@/domain/errors'
 import { LoadMaster } from '@/domain/usecases'
 
-export class RemoteLoadMaster {
+export class RemoteLoadMaster implements LoadMaster {
   constructor(
     private readonly httpGetClient: HttpGetClient<RemoteLoadMaster.Model>
   ) {}
 
-  async load(): Promise<void> {
+  async load(): Promise<LoadMaster.Model> {
     const httpResponse = await Promise.race([
       this.httpGetClient.get('/people/1'),
       this.httpGetClient.get('/people/4')
     ])
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        break
+        return httpResponse.body
       default:
         throw new UnexpectedError()
     }
