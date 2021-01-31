f0928bc2df09e20a270360e6789b80437d0f3f80
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-31 01:25:50 -0400

    chore: add webpack prod configs

---
+++ webpack.prod.js
@@ -0,0 +1,53 @@
+const { DefinePlugin } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const common = require('./webpack.common')
+const { merge } = require('webpack-merge')
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  externals: {
+    react: 'React',
+    axios: 'axios',
+    'react-dom': 'ReactDOM',
+    'react-router-dom': 'ReactRouterDOM'
+  },
+  plugins: [
+    new DefinePlugin({
+      'process.env.API_URL': JSON.stringify('https://swapi.dev/api')
+    }),
+    new HtmlWebpackPlugin({
+      template: './template-prod.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main-bundle-[hash].css'
+    })
+  ]
+})
