2844fac01ce371c0e5dd0e9170f5412128024b97
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-30 21:21:11 -0400

    refactor: move sut creation to a factory method

--- src/presentation/pages/your-master/your-master.spec.tsx
+++ src/presentation/pages/your-master/your-master.spec.tsx
@@ -4,14 +4,26 @@ import YourMaster from './your-master'
 import { LoadMasterSpy } from '@/domain/test/mock-load-master'
 import { AppProvider } from '@/presentation/contexts'
 
+type SutTypes = {
+  loadMasterSpy: LoadMasterSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadMasterSpy = new LoadMasterSpy()
+  render(
+    <AppProvider loadMaster={loadMasterSpy}>
+      <YourMaster />
+    </AppProvider>
+  )
+
+  return {
+    loadMasterSpy
+  }
+}
+
 describe('YourMaster Component', () => {
   test('Should call LoadMaster choose button click', () => {
-    const loadMasterSpy = new LoadMasterSpy()
-    render(
-      <AppProvider loadMaster={loadMasterSpy}>
-        <YourMaster />
-      </AppProvider>
-    )
+    const { loadMasterSpy } = makeSut()
     fireEvent.click(screen.getByText(/^choose your path/))
     expect(loadMasterSpy.callsCount).toBe(1)
   })
