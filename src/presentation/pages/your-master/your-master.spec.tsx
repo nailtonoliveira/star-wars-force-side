06a2e1c7d286c9f272104066b06b3b93fbf0dbb3
Nailton Oliveira <nailton.oliveira@vtinova.com.br>
2021-01-30 21:46:59 -0400

    test: ensure render correctly on state isLoading

--- src/presentation/pages/your-master/your-master.spec.tsx
+++ src/presentation/pages/your-master/your-master.spec.tsx
@@ -3,6 +3,7 @@ import { fireEvent, render, screen } from '@testing-library/react'
 import YourMaster from './your-master'
 import { LoadMasterSpy } from '@/domain/test/mock-load-master'
 import { AppProvider } from '@/presentation/contexts'
+import { IconNames } from '@/presentation/components/icon/icon'
 
 type SutTypes = {
   loadMasterSpy: LoadMasterSpy
@@ -26,5 +27,21 @@ describe('YourMaster Component', () => {
     const { loadMasterSpy } = makeSut()
     fireEvent.click(screen.getByText(/^choose your path/))
     expect(loadMasterSpy.callsCount).toBe(1)
+  })
+
+  test('Should render correctly on state isLoading', () => {
+    makeSut()
+    fireEvent.click(screen.getByText(/^choose your path/))
+
+    expect(screen.getByTestId('your-master-wrap')).toHaveAttribute(
+      'choosing-force',
+      'yes'
+    )
+    expect(screen.getByTestId('icon')).toHaveProperty('src', IconNames.black)
+    expect(screen.getByText(/^choose your path/)).toBeDisabled()
+    expect(screen.queryByTestId('master-image')).not.toBeInTheDocument()
+    expect(screen.getByTestId('image-skeleton')).toBeInTheDocument()
+    expect(screen.queryByTestId('master-text')).not.toBeInTheDocument()
+    expect(screen.getByTestId('text-skeleton')).toBeInTheDocument()
   })
 })
